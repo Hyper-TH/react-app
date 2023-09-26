141acdc479f7b2e07a7aab86b76aecbd89e9a210
Hyper-TH <twila.shayne.habab@outlook.com>
2023-09-26 20:45:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      {/*Call component*/}
+      <User name="Hyper" age={21} email="hyper@hyper.com"/>
+      <User name="Max" age={21} email="max@max.com"/>
+
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
+}
+
+// JS Function
+// Returns primitive JS type
+// const getName = () => {
+//   return "Hyper";
+// };
+
+// React component
+// Always return UI stuff (must be capitalized)
+const GetNameComponent = () => {
+  return <h1>Hyper</h1>;
+};
+
+/*
+const props = {
+  name: "Pedro",
+  age: 21,
+  email: "hyper@hyper.com"
+}
+*/
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
 }
 
 export default App;
