e2d635ccb49cb6f08b38b0acecb545b171398ec1
Hyper-TH <twila.shayne.habab@outlook.com>
2023-09-29 23:04:16 +0100

    Corrected list

--- src/App5.js
+++ src/App5.js
@@ -6,7 +6,7 @@ function App() {
     const planets = [
         { name: "Mars", isGasPlanet: false},
         { name: "Earth", isGasPlanet: false},
-        { name: "Jupiter", isGasPlanet: false},
+        { name: "Jupiter", isGasPlanet: true},
         { name: "Venus", isGasPlanet: false},
         { name: "Neptune", isGasPlanet: true},
         { name: "Uranus", isGasPlanet: true}
