cff0928908657b2f4fd0c75bdb5da07cbf5946c8
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-07 21:00:34 +0100

    Added Profile link

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,7 @@ export const Navbar = () => {
         <div> 
             <Link to="/"> Home </Link>
             <Link to="/menu"> Menu </Link>
+            <Link to="/profile"> Profile </Link>
             <Link to="/contact"> Contact </Link>
         </div>
     );
