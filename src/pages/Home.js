57b41065ecc0fbf21224a6844a9a7149e6e3e139
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-08 13:54:43 +0100

    Modified for React Query Module App18

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
-import { useContext } from 'react';
-import { AppContext } from '../App17';
+// import { useContext } from 'react';
+// import { AppContext } from '../App17';
+import { useQuery } from '@tanstack/react-query';
+import Axios from 'axios';
 
-// Props becomes eliminated
 export const Home = () => {
-    const { username } = useContext(AppContext)
-    return <h1>THIS IS THE HOMEPAGE AND USER IS: {username}</h1>;
+    // const { username } = useContext(AppContext)
+    // return <h1>THIS IS THE HOMEPAGE AND USER IS: {username}</h1>;
+    const { 
+            data: catData, 
+            isLoading, 
+            isError, 
+            refetch } = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+    });
+
+    if (isError) {
+        return <h1> Sorry, there was an error </h1>;
+    };
+
+    if (isLoading) {
+        return <h1> Loading... </h1>;
+    };
+
+    return (
+        <h1>
+            This is the home page 
+            <p> {catData?.fact} </p>
+            <button onClick={refetch}> Update Data </button>
+        </h1>
+    );
 }
