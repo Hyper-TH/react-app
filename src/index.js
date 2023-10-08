88f6baf17432d2c0455469d7f8da66d179af9ebe
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-08 13:54:57 +0100

    Enabled Strict Mode for App18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App15';
+import App from './App18';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
 
